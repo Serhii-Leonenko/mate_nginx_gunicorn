ddc907471296421a2a6ab8542a692f95f4dfb6fd
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-07-19 21:21:49 +0300

    add urls dish type

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,33 @@
 from django.urls import path
 
-from kitchen.views import index
+from kitchen.views import index, DishTypeListView, DishTypeCreateView, DishTypeUpdateView, DishTypeDeleteView
 
 urlpatterns = [
-    path("", index, name="index"),
+    path(
+        "",
+        index,
+        name="index"
+    ),
+    path(
+        "dish_type/",
+        DishTypeListView.as_view(),
+        name="dish-type-list",
+    ),
+    path(
+        "dish_type/create/",
+        DishTypeCreateView.as_view(),
+        name="dish-type-create",
+    ),
+    path(
+        "manufacturers/<int:pk>/update/",
+        DishTypeUpdateView.as_view(),
+        name="dish-type-update",
+    ),
+    path(
+        "manufacturers/<int:pk>/delete/",
+        DishTypeDeleteView.as_view(),
+        name="dish-type-delete",
+    ),
 ]
 
 app_name = "kitchen"
