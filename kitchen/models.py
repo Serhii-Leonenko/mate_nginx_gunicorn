2d4a7b06b6d18354364e7645a6b558080069e1e3
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-07-19 14:20:11 +0300

    add model Cook

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -9,4 +10,17 @@ class DishType(models.Model):
 
     def __str__(self) -> str:
         return f"{self.name}"
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
 
