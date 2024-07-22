e979d856274902845c49abd5a3841b39f606e773
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-07-23 00:32:05 +0300

    delete ordering from DyshType

--- kitchen/models.py
+++ kitchen/models.py
@@ -6,9 +6,6 @@ from django.db import models
 class DishType(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
-    class Meta:
-        ordering = ["name"]
-
     def __str__(self) -> str:
         return f"{self.name}"
 
