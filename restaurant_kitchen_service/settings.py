9c019e0f964f590ad69dac8982c27231cca5a20d
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-07-24 03:03:21 +0300

    fix ALLOWED_HOSTS

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://restaurant-kitchen-service-u7vs.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "restaurant-kitchen-service-u7vs.onrender.com"]
 
 # Application definition
 
